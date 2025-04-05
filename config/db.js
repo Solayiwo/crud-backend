542e8ab085f211c1e1438ac404ee3ad8975ea604
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2025-04-05 23:41:22 +0100

    Database configuration

--- config/db.js
+++ config/db.js
@@ -3,12 +3,19 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Supabase and many managed DBs
+  },
 });
 
 module.exports = pool;
