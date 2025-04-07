f0c92f0251395f45226bb56a22d777c847467171
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2025-04-07 19:02:36 +0100

    Modified

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const app = require("../server"); // Import the Express app
 const serverless = require("serverless-http"); // Adapter to make Express compatible
 
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
